72d6cfba754de7f6ab2fd4d1550a2d25a772d98b
Ratheesh Ramkumar(UST <U61717@ust-global.com>
2022-08-16 12:25:21 +0530

    single-spa microfrontends intial commit

---
+++ app-details/src/app/service/demo.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class DemoService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllCountry(): Observable<any> {
+    return this.http.get('https://restcountries.com/v2/all');
+  }
+
+  getCountryDetails(countryName: string): Observable<any> {
+    // return this.http.get(`https://restcountries.eu/rest/v2/name/${countryName}`);
+    return this.http.get(`https://restcountries.com/v2/name/${countryName}`);
+  }
+}
