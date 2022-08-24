dd657023f39c1d99a3b5200022008f318256daab
Ratheesh Ramkumar(UST <U61717@ust-global.com>
2022-08-24 13:55:14 +0530

    angular app added

---
+++ Angular-app/src/app/data.ts
@@ -0,0 +1,133 @@
+export const data = [
+  {
+    "name": "NGO/Implementing Organization",
+    "key": "NGO/Implementing Organization",
+    "data-buildertype": "input",
+    "label": "NGO/Implementing Organization",
+    "placeholder": "NGO/Implementing Organization",
+    "validators": {
+      "required": true
+    }
+  },
+  {
+    "name": "Primary Contact Name",
+    "key": "Primary Contact Name / Project Lead",
+    "data-buildertype": "input",
+    "label": "Primary Contact Name / Project Lead",
+    "placeholder": "Primary Contact Name / Project Lead",
+    "validators": {
+      "required": true
+    }
+  },
+  {
+    "name": "Primary Email Address",
+    "key": "Primary Email Address",
+    "data-buildertype": "input",
+    "label": "Primary Email Address",
+    "placeholder": "Primary Email Address",
+    "validators": {
+      "required": true
+    }
+  },
+  {
+    "name": "Secondary Contact Name",
+    "key": "Secondary Contact Name / Project Lead",
+    "data-buildertype": "input",
+    "label": "Secondary Contact Name / Project Lead",
+    "placeholder": "Secondary Contact Name / Project Lead",
+    "validators": {
+      "required": true
+    }
+  },
+  {
+    "key": "Secondary Email Address",
+    "data-buildertype": "input",
+    "label": "Secondary Email Address",
+    "placeholder": "Secondary Email Address",
+    "validators": {
+      "required": true
+    }
+  },
+  {
+    "name": "checkbox",
+    "key": "SCOPE & SCALE",
+    "label": "1. SCOPE & SCALE",
+    "data-buildertype": "checkbox",
+     "options": "'Sustainability and production-based goals are clearly stated and relevant to the landscape/seascape in which the program is being implemented','The program is of meaningful scale to drive improvements at the landscape/seascape-level','Not Applicable'",
+    "validators": {
+      "required": true
+    }
+  },
+  {
+    "label": "2. STAKEHOLDER ENGAGEMENT",
+    "data-buildertype": "checkbox",
+     "options": "'A representative, multi-stakeholder body is developed transparently and leads the program design and implementation','Relevant levels of government are engaged in developing and implementing approach/program','Not Applicable'",
+    "validators": {
+      "required": true
+    }
+  },
+  {
+    "label": "3. PROGRAM DESIGN",
+    "data-buildertype": "checkbox",
+    "options": "'A clearly defined action plan is developed that lays out steps to meet program milestones and outcomes','Meaningful, relevant metrics and KPIs are defined to enable assessments of progress towards targets and milestones','Effective data governance systems and protocols are implemented to credibly gather, store, analyze, and use data','Not Applicable'",
+    "validators": {
+      "required": true
+    }
+  },
+  {
+    "data-buildertype": "textarea",
+    "label": "4. Please briefly describe the initiative and key objectives. Note: This may be used as the public project description on the Walmart Sustainability hub website (300-word limit).*",
+    "element": "textarea",
+    "validators":{
+        "required": true
+    }
+  },
+  {
+    "data-buildertype": "textarea",
+    "label": "5. Please describe landscape-level goals shared with other stakeholders and how the initiative contributes to these landscape/seascape-level goals. *",
+    "element": "textarea",
+    "validators":{
+        "required": true
+    }
+  },
+  {
+    "data-buildertype": "textarea",
+    "label": "6. What are the key challenges, development problems, and issues related to conversion of natural ecosystems or degradation of natural resources this initiative is trying to address?",
+    "element": "textarea",
+    "validators":{
+        "required": true
+    }
+  },
+  {
+    "data-buildertype": "textarea",
+    "label": "7. Are other supply chain actors, including other retailers, suppliers consumer brands, manufacturers and processors, or producers (including smallholders) currently participating in this initiative? Please list them.",
+    "element": "textarea",
+    "validators":{
+        "required": true
+    }
+  },
+  {
+    "data-buildertype": "textarea",
+    "label": "8. Are other supply chain actors, including other retailers, suppliers consumer brands, manufacturers and processors, or producers (including smallholders) currently participating in this initiative? Please list them.",
+    "element": "textarea",
+    "validators":{
+        "required": true
+    }
+  },
+  {
+    "data-buildertype": "textarea",
+    "label": "9. Are other supply chain actors, including other retailers, suppliers consumer brands, manufacturers and processors, or producers (including smallholders) currently participating in this initiative? Please list them.",
+    "element": "textarea",
+    "validators":{
+        "required": true
+    }
+  },
+{
+  "data-buildertype": "textarea",
+  "label": "10. Please describe how this initiative will contribute to improving the livelihoods of local communities and safeguarding human rights:",
+  "element": "textarea",
+  "validators":{
+      "required": true
+  }
+}
+ ];
---
+++ Angular-app/src/app/empty-route/empty-route.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-route',
+  template: '<div>Empty routes</div>',
+})
+export class EmptyRouteComponent {}
